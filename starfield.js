ab220a1c992ff33bde166235b0385911fbe30c41
nbogie <neillbogie@googlemail.com>
2022-03-01 13:50:43 +0000

    added basic p5.js demo to the website

--- starfield.js
+++ starfield.js
@@ -1,1 +1,14 @@
+//@ts-nocheck
+
 console.log("HELLO FROM JS");
+
+function setup() {
+  console.log("SETUP HAS BEEN CALLED");
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background("white");
+  fill("green");
+  square(frameCount % 500, 200, 100);
+}
